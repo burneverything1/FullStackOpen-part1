ca5d3fbf0e853e23583dd1ecbc44948afc7abc09
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-03-25 21:57:31 -0400

    part 1.6

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
-}
+const Display = (props) => <p>{props.text} {props.value}</p>
 
-const Part = (props) => {
-  return <p>{props.part} {props.exercise}</p>
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
-      <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
-      <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodHandler = () => setGood(good + 1)
+  const neutralHandler = () => setNeutral(neutral + 1)
+  const badHandler = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={goodHandler}>good</button>
+        <button onClick={neutralHandler}>neutral</button>
+        <button onClick={badHandler}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <Display text={'good'} value={good}/>
+      <Display text={'neutral'} value={neutral}/>
+      <Display text={'bad'} value={bad}/>
     </div>
   )
 }
